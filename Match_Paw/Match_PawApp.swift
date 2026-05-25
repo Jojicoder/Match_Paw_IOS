cf0d4ed9b267c0d7a48eb8025aed277cd52fef20
Jojicoder <joji.kashimura@live.lagcc.cuny.edu>
2026-05-25 14:29:50 -0400

    Initial Commit

---
+++ Match_Paw/Match_PawApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_PawApp.swift
+//  Match_Paw
+//
+//  Created by Joji Kashimura on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Match_PawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Match_PawTests/Match_PawTests.swift
@@ -0,0 +1,19 @@
+//
+//  Match_PawTests.swift
+//  Match_PawTests
+//
+//  Created by Joji Kashimura on 5/25/26.
+//
+
+import Testing
+@testable import Match_Paw
+
+struct Match_PawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
